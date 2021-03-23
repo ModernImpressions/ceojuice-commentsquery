20829daaf99831f8f82be809f2a7f9970208f56d
Patrick Barnhardt <contact@crimsonstrifegames.com>
2021-03-23 15:41:01 -0400

    Update testimonials_part.php

--- testimonials_part.php
+++ testimonials_part.php
@@ -1,10 +1,21 @@
 <?php
+$ceoURL = "https://api.ceojuice.com/api/Processes/SurveyComments?CustomerNumber=";
+$ceoCustNum = "097";
+$ceoAPIKey = "dc5919d9-d0fb-44e0-a6d2-d71e8f38e747";
+$ceoqty = "10";
 
-$posts = file_get_contents('https://api.ceojuice.com/api/Processes/SurveyComments?CustomerNumber=097&AuthKey=dc5919d9-d0fb-44e0-a6d2-d71e8f38e747&qty=10');
-
-// do something w/ $posts here....
-
-echo $posts;
-
+$posts = @file_get_contents($ceoURL.$ceoCustNum.'&AuthKey='.$ceoAPIKey.'&qty='.$ceoqty, true);
+if ($posts === false) {
+    //There is an error opening the file
+    echo "failed to poll data";
 ?>
-This is a test
+  <div class="slide">
+    <blockquote>
+    <span class="leftq quotes">&ldquo;</span>comment<span class="rightq quotes">&bdquo;</span>
+    </blockquote>
+    <h2>John Doe</h2>
+    <h6>Developer Relations at Woof Studios</h6>
+  </div>
+<?php } else {
+    echo $posts;
+}?>
