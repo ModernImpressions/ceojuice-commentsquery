c3a9a5d8e259d39632cf3c8522003a2a6a310ede
Patrick Barnhardt <contact@patrickbarnhardt.info>
2022-04-19 20:37:37 -0400

    Update testimonials_part.php

--- testimonials_part.php
+++ testimonials_part.php
@@ -5,12 +5,12 @@ $ceoAPIKey = ot_get_option('ceojuice_apiauth');
 $ceoqty = ot_get_option('ceojuice_commentcount');
 $slideId = 1;
 //$format = new NumberFormatter("en", NumberFormatter::SPELLOUT);
-$buildURL = $ceoURL.$ceoCustNum.'&AuthKey='.$ceoAPIKey.'&qty='.$ceoqty;
+$buildURL = $ceoURL . $ceoCustNum . '&AuthKey=' . $ceoAPIKey . '&qty=' . $ceoqty;
 
 $readjson = @file_get_contents($buildURL, true);
 if ($readjson === false) {
     //There is an error opening the file
-?>    
+?>
 <?php } else {
     //Results are recieved from the CEOJuice API
     //Decode JSON
@@ -24,25 +24,25 @@ if ($readjson === false) {
         //placeholder for any future attempts to process company names
         $companyName = $companyId;
         echo "<div class='item'>";
-        echo "<!--TESTIMONIAL ".$slideId."-->";
+        echo "<!--TESTIMONIAL " . $slideId . "-->";
         echo "<div class='testimonial-slide'>";
         echo "<blockquote class='customer-blockquote'>";
         echo "<span class='leftq quotes'>&ldquo;</span>";
-        echo "<p id='comment-text'>".$comment."</p>";
+        echo "<p id='comment-text'>" . $comment . "</p>";
         echo "<span class='rightq quotes'>&bdquo;</span>";
         echo "</blockquote>";
         echo "<span class='contact-citation'>";
-        echo "<h2 class='customername testimonial-name'>".$contact."</h2>";
-        echo "<h6 class='surveydate date'>".date_format($date, "m/d/Y")."</h6>";
+        echo "<h2 class='customername testimonial-name'>" . $contact . "</h2>";
+        echo "<h6 class='surveydate date'>" . date_format($date, "m/d/Y") . "</h6>";
         if ($companyName == null) {
             //return nothing if no company name is available
         } else {
-            echo "<h6 class ='companyname'> - ".$companyName."</h6>";
+            echo "<h6 class ='companyname'> - " . $companyName . "</h6>";
         }
         echo "</span>";
         echo "</div>";
-        echo "<!--END OF TESTIMONIAL ".$slideId."-->";
+        echo "<!--END OF TESTIMONIAL " . $slideId . "-->";
         echo "</div>";
         $slideId++;
     }
-}?>
+} ?>
